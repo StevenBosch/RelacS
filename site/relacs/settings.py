1adfe6678c70fb2d0a3e618edf0fbe966157f513
XeryusTC <armadillo@onenetbeyond.org>
2016-05-26 22:41:03 +0200

    Show something nice on the website

--- site/relacs/settings.py
+++ site/relacs/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR.child('static')
 STATICFILES_DIRS = (
     BASE_DIR.child('static'),
 )
