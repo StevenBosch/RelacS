f1ac0dca73ddc772cbdc265aa8be097c078d75b6
XeryusTC <armadillo@onenetbeyond.org>
2016-05-27 16:39:41 +0200

    Update models

--- site/results/models.py
+++ site/results/models.py
@@ -4,16 +4,33 @@ from django.db import models
 
 class Recording(models.Model):
     filename = models.CharField(max_length=255)
-    length = models.IntegerField
+    length = models.IntegerField()
+    stressful = models.IntegerField()
+    relaxing = models.IntegerField()
 
+    def __str__(self):
+        return self.filename
 
 class Sound(models.Model):
     recording = models.ForeignKey(Recording)
     start = models.IntegerField()
     end = models.IntegerField()
-    stressful = models.FloatField()
-    relaxing = models.FloatField()
+    stressful = models.IntegerField()
+    relaxing = models.IntegerField()
+
+    def __str__(self):
+        return self.recording.filename + '-' + str(self.pk)
+
+    class Meta:
+        ordering = ('-stressful',)
 
 class Category(models.Model):
-    recording = models.ForeignKey(Sound)
+    sound = models.ForeignKey(Sound)
     weight = models.FloatField()
+    text = models.CharField(max_length=32)
+
+    def __str__(self):
+        return str(self.sound) + '-' + self.text
+
+    class Meta:
+        verbose_name_plural = 'categories'
