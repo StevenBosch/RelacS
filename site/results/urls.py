aa251a33baa47c1fead31af84ed59b424168bc5b
XeryusTC <armadillo@onenetbeyond.org>
2016-05-27 15:26:30 +0200

    Add page for results

--- site/results/urls.py
+++ site/results/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^results/$', views.ResultsView.as_view(), name='results'),
 ]
--- site/results/views.py
+++ site/results/views.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'results/index.html'
 
-def index(request):
-    return HttpResponse("Hello, world")
+
+class ResultsView(TemplateView):
+    template_name = 'results/results.html'
