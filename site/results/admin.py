b020d206da316e8e77e016c7b0b36f66f463abe9
XeryusTC <armadillo@onenetbeyond.org>
2016-05-27 15:30:33 +0200

    Create models and their admin pages

--- site/results/admin.py
+++ site/results/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from results import models
 
-# Register your models here.
+class RecordingAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Recording, RecordingAdmin)
+
+class SoundAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Sound, SoundAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(models.Category, CategoryAdmin)
--- site/results/models.py
+++ site/results/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Recording(models.Model):
+    filename = models.CharField(max_length=255)
+    length = models.IntegerField
+
+
+class Sound(models.Model):
+    recording = models.ForeignKey(Recording)
+    start = models.IntegerField()
+    end = models.IntegerField()
+    stressful = models.FloatField()
+    relaxing = models.FloatField()
+
+class Category(models.Model):
+    recording = models.ForeignKey(Sound)
+    weight = models.FloatField()
